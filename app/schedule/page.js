0e4d233065681eb25d73b992891f4bb2033432f1
IshmamF <ishmamf2003@gmail.com>
2024-06-22 13:41:13 -0400

    next js initialization

---
+++ app/schedule/page.js
@@ -0,0 +1,13 @@
+import { Navbar } from "../components/Navbar"
+
+export default function Page() {
+  return (
+    <div>
+        <Navbar />
+        <div>
+            Page
+        </div>
+    </div>
+  )
+}
+
